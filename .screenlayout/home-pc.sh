505d3c1faa96507b146b664d46c0b5e02c2a7a92
jessun <jessun.zhang@outlook.com>
2022-06-21 07:03:14 +0800

    feat(screenlayout): add screenlayout of home-pc.

---
+++ .screenlayout/home-pc.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x136 --rotate left --output DP-0 --primary --mode 2560x1440 --pos 1080x0 --rotate normal --output DP-1 --off --output DVI-D-1 --off
