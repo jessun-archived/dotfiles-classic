997ddadee09803eab22bf8de024c1eccdd20804f
jessun <jessun.zhang@outlook.com>
2022-09-13 14:05:06 +0800

    update(nvim): update keymaps

--- .config/nvim/lua/plugins/configs/hop-nvim.lua
+++ .config/nvim/lua/plugins/configs/hop-nvim.lua
@@ -7,9 +7,3 @@ vim.api.nvim_set_keymap('', 'f',
 vim.api.nvim_set_keymap('', 'F',
     "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false })<cr>"
     , {})
-vim.api.nvim_set_keymap('', 't',
-    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })<cr>"
-    , {})
-vim.api.nvim_set_keymap('', 'T',
-    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })<cr>"
-    , {})
