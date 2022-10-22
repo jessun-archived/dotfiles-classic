7cafe0342ec6491d4d77dc759e1b1c58bad47f76
jessun <jessun.zhang@outlook.com>
2022-10-22 12:12:06 +0800

    refactor(nvim): refactor packer.lua

--- .config/nvim/lua/plugins/packer.lua
+++ .config/nvim/lua/plugins/packer.lua
@@ -22,96 +22,101 @@ function Packer:start_up()
 	packer.startup(function()
 		--||||||||||||||||||||||||||||||||||||||||| 插件管理 ||||||||||||||||||||||||||||||||||||||||||||
 		use("wbthomason/packer.nvim") -------------------------------------------------------------------- 插件管理
-		use("neoclide/coc.nvim") ------------------------------------------------------------------------- coc.nvim
+		use({
+			"neoclide/coc.nvim",
+			run = "yarn install",
+		}) ------------------------------------------------------------------------- coc.nvim
+		use("nathom/filetype.nvim") ---------------------------------------------------------------------- filetype 性能优化
 		--
 		--
 		--|||||||||||||||||||||||||||||||||||||||||||| UI |||||||||||||||||||||||||||||||||||||||||||||||
 		use("kyazdani42/nvim-web-devicons") -------------------------------------------------------------- 扩展图标
 		use({ "folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim" }) -------------------------- 高亮关键字
-		use({ "jessun/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } }) -- 状态栏
+		use({ "jessun/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } }) -------- 状态栏
 		use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }) ------------------------------------ 代码高亮
-		use("neoclide/jsonc.vim")
-		use("petertriho/nvim-scrollbar")
-		use("koenverburg/peepsight.nvim")
+		use("petertriho/nvim-scrollbar") ----------------------------------------------------------------- 滚动条
+		use("koenverburg/peepsight.nvim") ---------------------------------------------------------------- 视觉聚焦
+		use("yaocccc/nvim-hlchunk") ---------------------------------------------------------------------- 线条当前块
+		use({
+			"kyazdani42/nvim-tree.lua", ------------------------------------------------------------------ 文件树
+			requires = {
+				"kyazdani42/nvim-web-devicons", -- optional, for file icons
+			},
+			tag = "nightly", -- optional, updated every week. (see issue #1193)
+		})
 
 		---------------------------- colorscheme {
 		use("arcticicestudio/nord-vim")
-		-- use("flazz/vim-colorschemes")
-		-- use("jessun/vimspectr")
-		-- use("mcchrish/vim-no-color-collections")
-		-- use("mkarmona/colorsbox")
-		-- use("mswift42/vim-themes")
-		-- use("projekt0n/github-nvim-theme")
-		-- use("rafi/awesome-vim-colorschemes")
-		-- use("rainglow/vim")
-		-- use("rmehri01/onenord.nvim")
+		use("karoliskoncevicius/sacredforest-vim")
+		use("flazz/vim-colorschemes")
+		use("jessun/vimspectr")
+		use("mcchrish/vim-no-color-collections")
+		use("mkarmona/colorsbox")
+		use("mswift42/vim-themes")
+		use("projekt0n/github-nvim-theme")
+		use("rafi/awesome-vim-colorschemes")
+		use("rainglow/vim")
+		use("rmehri01/onenord.nvim")
 		use("shaunsingh/nord.nvim")
-		-- use("easysid/mod8.vim")
+		use("easysid/mod8.vim")
 		---------------------------- colorscheme }
-
-		--||||||||||||||||||||||||||||||||||||||||| 文本操作 ||||||||||||||||||||||||||||||||||||||||||||
-		use("machakann/vim-sandwich") ---------------------=---------------------------------------------- 快速两端文本 ?
-		use("github/copilot.vim") ------------------------------------------------------------------------ Github AI 补全
+		--
+		--
+		--||||||||||||||||||||||||||||||||||||||||| 编码工具 ||||||||||||||||||||||||||||||||||||||||||||
 		use({
-			"iamcco/markdown-preview.nvim",
+			"iamcco/markdown-preview.nvim", ------------------------------------------------------------- markdown 预览
 			run = "cd app && npm install",
 			setup = function()
 				vim.g.mkdp_filetypes = { "markdown" }
 			end,
 			ft = { "markdown" },
 		})
+		use("numToStr/Comment.nvim") -------------------------------------------------------------------- 代码注释
+		use("liuchengxu/vista.vim") --------------------------------------------------------------------- 代码大纲
+		use("neoclide/jsonc.vim") ----------------------------------------------------------------------- jsonc 文件高亮
 		--
-		--
-		--||||||||||||||||||||||||||||||||||||||||| 辅助工具 ||||||||||||||||||||||||||||||||||||||||||||
-		use("dstein64/vim-startuptime")
+
+		--||||||||||||||||||||||||||||||||||||||||| 文本操作 ||||||||||||||||||||||||||||||||||||||||||||
+		use("github/copilot.vim") ----------------------------------------------------------------------- Github AI 补全
 		--
 		--
 		--||||||||||||||||||||||||||||||||||||||||| 目录管理 ||||||||||||||||||||||||||||||||||||||||||||
-		use({
-			"kyazdani42/nvim-tree.lua",
-			requires = {
-				"kyazdani42/nvim-web-devicons", -- optional, for file icons
-			},
-			tag = "nightly", -- optional, updated every week. (see issue #1193)
-		})
+		--
+		--
+		--||||||||||||||||||||||||||||||||||||||||| 辅助工具 ||||||||||||||||||||||||||||||||||||||||||||
+		use("dstein64/vim-startuptime") ----------------------------------------------------------------- 启动时间
+		--
 		--
 		--||||||||||||||||||||||||||||||||||||||||||| GIT |||||||||||||||||||||||||||||||||||||||||||||||
-		-- use {"sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim"} --------------------------- 差异比较
-		--
-		--
-		--||||||||||||||||||||||||||||||||||||||||| 编码工具 ||||||||||||||||||||||||||||||||||||||||||||
-		-- use "stevearc/aerial.nvim" ---------------------------------------------------------------------- 代码大纲
-		use("numToStr/Comment.nvim") --------------------------------------------------------------------- 代码注释
-		use("liuchengxu/vista.vim")
 		--
 		--
 		--||||||||||||||||||||||||||||||||||||||||| 模糊搜索 ||||||||||||||||||||||||||||||||||||||||||||
-		use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } }) ------------- 模糊搜索
-		-- use "danielpieper/telescope-tmuxinator.nvim" ---------------------------------------------------- tmuxinator 跳转
-		use("eckon/treesitter-current-functions") -------------------------------------------------------- 当前函数 ?
-		use("nvim-treesitter/nvim-treesitter-textobjects") ----------------------------------------------- 文本对象 ?
-		use("RRethy/nvim-treesitter-textsubjects") ------------------------------------------------------- 文本对象 ?
-		use("nvim-treesitter/nvim-treesitter-context")
+		use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } }) ------------ 模糊搜索
 		--
 		--||||||||||||||||||||||||||||||||||||||||| 性能优化 ||||||||||||||||||||||||||||||||||||||||||||
-		use("nathom/filetype.nvim") ---------------------------------------------------------------------- filetype 性能优化
 
-		--||||||||||||||||||||||||||||||||||||||||| TESTING ||||||||||||||||||||||||||||||||||||||||||||
-		-- use("lukas-reineke/indent-blankline.nvim")
+		--||||||||||||||||||||||||||||||||||||||||| TESTING |||||||||||||||||||||||||||||||||||||||||||||
 		use({
 			"lewis6991/gitsigns.nvim",
 			-- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
 		})
 		use({ "anuvyklack/windows.nvim", requires = { "anuvyklack/middleclass", "anuvyklack/animation.nvim" } })
-		use("yaocccc/nvim-hlchunk")
 		use("smjonas/live-command.nvim")
 		use("tpope/vim-fugitive")
 		use({ "akinsho/git-conflict.nvim", tag = "*" })
 		use("justinmk/vim-sneak")
 		use("karb94/neoscroll.nvim")
+		use("eckon/treesitter-current-functions") -------------------------------------------------------- 当前函数 ?
+		use("nvim-treesitter/nvim-treesitter-textobjects") ----------------------------------------------- 文本对象 ?
+		use("RRethy/nvim-treesitter-textsubjects") ------------------------------------------------------- 文本对象 ?
+		use("nvim-treesitter/nvim-treesitter-context")
+		use("machakann/vim-sandwich") ---------------------=---------------------------------------------- 快速两端文本 ?
+		-- use {"sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim"} --------------------------- 差异比较
 
 		--||||||||||||||||||||||||||||||||||||||||| DEPRECATED ||||||||||||||||||||||||||||||||||||||||||||
+		-- use "danielpieper/telescope-tmuxinator.nvim" -------------------------------------------------- tmuxinator 跳转
 		-- use("sunjon/Shade.nvim")
+		-- use("lukas-reineke/indent-blankline.nvim")
 	end)
 end
 
