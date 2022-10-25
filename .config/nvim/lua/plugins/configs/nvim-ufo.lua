51061e297191317cc58185d71e1fab68d1603a56
jessun <jessun.zhang@outlook.com>
2022-10-25 20:41:02 +0800

    fix(nvim): update nvim-ufo config

--- .config/nvim/lua/plugins/configs/nvim-ufo.lua
+++ .config/nvim/lua/plugins/configs/nvim-ufo.lua
@@ -20,7 +20,7 @@ require('ufo').setup({
     provider_selector = function(bufnr, filetype, buftype)
         -- if you prefer treesitter provider rather than lsp,
         -- return ftMap[filetype] or {'treesitter', 'indent'}
-        return { 'treesitter', 'indent' }
+        return { 'treesitter' }
 
         -- refer to ./doc/example.lua for detail
     end
