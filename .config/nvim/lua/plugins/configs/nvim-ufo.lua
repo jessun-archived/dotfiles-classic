1baadf6b7cce342f14a4f3d8a3e968a185ca9edc
jessun <jessun.zhang@outlook.com>
2022-12-19 17:43:36 +0800

    fix(nvim): update plugin nvim-ufo

--- .config/nvim/lua/plugins/configs/nvim-ufo.lua
+++ .config/nvim/lua/plugins/configs/nvim-ufo.lua
@@ -1,29 +1,4 @@
-local ftMap = {
-    vim = 'indent',
-    python = { 'indent' },
-    git = ''
-}
 require('ufo').setup({
-    open_fold_hl_timeout = 150,
-    close_fold_kinds = { 'imports', 'comment' },
-    preview = {
-        win_config = {
-            border = { '', '─', '', '', '', '─', '', '' },
-            winhighlight = 'Normal:Folded',
-            winblend = 0
-        },
-        mappings = {
-            scrollU = '<C-u>',
-            scrollD = '<C-d>'
-        }
-    },
-    provider_selector = function(bufnr, filetype, buftype)
-        -- if you prefer treesitter provider rather than lsp,
-        -- return ftMap[filetype] or {'treesitter', 'indent'}
-        return { 'treesitter' }
-
-        -- refer to ./doc/example.lua for detail
-    end
 })
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
